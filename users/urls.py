afc1f34b6d766e755adf0d9790a85b3a7a0d7bc3
Ghada Emad <ghadaemad011@gmail.com>
2025-05-21 02:59:09 +0300

    handle activation email after user register

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name='users'
 urlpatterns = [
     path('signup/',views.signUp,name='signup'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('activate_email/',views.activate_page,name="activateMessage"),
     path('login/',views.login,name='login')
     
 ]
