79b0dc7aff036a65b8840a2bada3ed1e394d92b1
Ghada Emad <ghadaemad011@gmail.com>
2025-05-21 00:14:00 +0300

    try mysql and change it if require and add urls for users in core project

--- crowdFunders/settings.py
+++ crowdFunders/settings.py
@@ -32,8 +32,16 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # Database configuration
 DATABASES = {
-     'default': env.db(default='sqlite:///db.sqlite3'),
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DBNAME'),
+        'USER': env('USER'),
+        'PASSWORD': env('PASSWORD'),
+        'HOST': env('HOST'),
+        'PORT': env('PORT'),
+    }
 }
+
 
 
 from pathlib import Path
@@ -103,12 +111,6 @@ WSGI_APPLICATION = 'crowdFunders.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
