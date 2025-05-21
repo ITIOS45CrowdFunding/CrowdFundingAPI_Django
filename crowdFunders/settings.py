3b0d46c7b513044ddb0d3addedc9f3adcf352e88
Ghada Emad <ghadaemad011@gmail.com>
2025-05-21 03:00:04 +0300

    add smtp email to send email to user

--- crowdFunders/settings.py
+++ crowdFunders/settings.py
@@ -168,3 +168,11 @@ TAILWIND_APP_NAME = 'theme'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env('EMAIL_HOST')
+EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
+EMAIL_USE_TLS = True
