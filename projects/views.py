d0f8360391efa4a58c3435aaa97af0b578590123
Ahmed-Elimam <aa.emam97@gmail.com>
2025-05-21 06:51:57 +0300

    projects view functions skeleton

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.db import models
 
 # Create your views here.
+def index(request):
+    pass
+
+def create_project(request):
+    pass
+
+def details(request,project_id):
+    pass
+
+def update_project(request,project_id):
+    pass
+
+def cancel_project(request,project_id):
+    pass
+
+def donate(request,project_id):
+    pass
+
+def add_comment(request,project_id):
+    pass
+
+def report_project(request,project_id):
+    pass
+
+def rate_project(request,project_id):
+    pass
