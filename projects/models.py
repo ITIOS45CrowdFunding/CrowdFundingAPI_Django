335262f914a94f9a8ad5cb344e97b4dbe7adf472
Ghada Emad <ghadaemad011@gmail.com>
2025-05-22 00:47:55 +0300

    change something in projects from start _ forget to delete it

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 class ProjectCategory(models.Model):
     name = models.CharField(max_length=255)
 
@@ -13,7 +13,7 @@ class Tag(models.Model):
         return self.name
 
 class Project(models.Model):
-    user = models.ForeignKey('users.User', on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     details = models.TextField()
     target = models.IntegerField()
@@ -41,20 +41,20 @@ class ProjectImage(models.Model):
 
 class Donation(models.Model):
     project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True)
-    user = models.ForeignKey("users.User", on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     amount = models.DecimalField(max_digits=12, decimal_places=2)
 
 class Report(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     reason = models.TextField()
 
 class Rating(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     value = models.IntegerField()
 
 class Comment(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     text = models.TextField()
