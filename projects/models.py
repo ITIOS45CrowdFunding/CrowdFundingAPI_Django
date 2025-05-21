801f8d8e9a334a53229c85ed08bb6a55fe2480c5
Ghada Emad <ghadaemad011@gmail.com>
2025-05-22 00:57:12 +0300

    fix: used custom user so the model is in settings

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db.models import Avg, Sum, Count
 class ProjectCategory(models.Model):
     name = models.CharField(max_length=255)
@@ -16,7 +15,7 @@ class Tag(models.Model):
 
 class Project(models.Model):
 
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     details = models.TextField()
     target = models.IntegerField()
@@ -77,23 +76,23 @@ class ProjectImage(models.Model):
 class Donation(models.Model):
     project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True)
 
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     amount = models.DecimalField(max_digits=12, decimal_places=2)
 
 class Report(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     reason = models.TextField()
 
 class Rating(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     value = models.IntegerField()
 
 class Comment(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     text = models.TextField()
